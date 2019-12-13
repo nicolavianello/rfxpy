123c52bd68375da3453604a7b56f3eaa0edca8db
Nicola <nicola.vianello@gmail.com>
2019-12-13 12:33:02 +0100

    added a signal_processing with cross and auto-correlation method

---
+++ rfx/utils/signal_processing.py
@@ -0,0 +1,23 @@
+import numpy as np
+from scipy import UnivariateSpline
+
+
+def correlation(x, y):
+    """
+    Compute the cross correlation function, return the lag x-values, the
+    normalized correlation and the correlation time defined as where it assumes 1/e
+    values of the maximum
+    Args:
+        x: first array
+        y: second array. Can be the same and then computes the auto-correlation time
+
+    Returns:
+        tuple with lag, correlation and correlation time
+    """
+
+    lag = np.arange(x.size, dtype="float") - x.size / 2.0
+    c = np.correlate(x, y, mode="same")
+    c /= np.sqrt(np.dot(x, x) * np.dot(y, y))
+    S = UnivariateSpline(lag, c - 1 / np.exp(1), s=0)
+    tac = S.roots()[S.roots() > 0]
+    return lag, c, tac
